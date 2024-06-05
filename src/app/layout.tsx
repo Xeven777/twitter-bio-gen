dddd83368d1e903daebcb374cf51d668b31cb290
Anish <xevenbiswas@gmail.com>
2024-06-05 17:26:59 +0530

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,19 @@ export default function RootLayout({
         <ThemeProvider attribute="class">
           <div className="absolute invisible dark:visible bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
           <TooltipProvider>{children}</TooltipProvider>
+          <footer className="p-4 relative text-center text-muted-foreground">
+            <p>
+              Made with ❤️ by{" "}
+              <a
+                href="https://dub.sh/anish-li"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-300 hover:underline"
+              >
+                Anish
+              </a>
+            </p>
+          </footer>
         </ThemeProvider>
       </body>
     </html>
